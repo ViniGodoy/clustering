054b6d274b57404b732d23c1f3669cb1db673b12
Vinícius Godoy de Mendonça <vinigodoy@gmail.com>
2022-05-12 10:34:23 -0300

    Added comments.

--- src/br/com/vinigodoy/samples/KMeansWithImage.java
+++ src/br/com/vinigodoy/samples/KMeansWithImage.java
@@ -1,6 +1,6 @@
 package br.com.vinigodoy.samples;
 
-import br.com.vinigodoy.clustering.type.color.*;
+import br.com.vinigodoy.clustering.data.image.*;
 
 import br.com.vinigodoy.clustering.kmeans.Kmeans;
 
@@ -11,13 +11,24 @@ public class KMeansWithImage {
     private static final int CLUSTERS = 20;
 
     public static void main(String[] args) throws Exception {
+        //We create an image data source to read image data
         final var ds = ImageDatasource.load("./images/field.jpg");
         final var name = "./images/kmeans-" + CLUSTERS + "-field";
+
+        //The image data source provide an easy way to create an image DataOutput, with the same dimensions of the
+        // original image
         try (var out = ds.createOutput(name)) {
+            //Create the kmeans object. Since we'll use images, we select the RGB solver.
             final var kmeans =
                     new Kmeans<>(new RGBSolver())
-                            .train(ds, CLUSTERS);
+                            .setCentroidSelector(ds.random()) //Use an efficient random centroid selector for images
+                            .train(ds, CLUSTERS); //Do the trainig
+
+            //Set the output color based on the closest point to each centroid. This will guarantee that only colors
+            //present in the image will be in the output. If you ommit this line, random colors will be used.
             out.setColorsByClosest(kmeans);
+
+            //Classify the image based in the output.
             kmeans.classify(ds, out);
         }
     }
--- src/br/com/vinigodoy/samples/KnnWithImage.java
+++ src/br/com/vinigodoy/samples/KnnWithImage.java
@@ -1,46 +1,62 @@
 package br.com.vinigodoy.samples;
 
 import br.com.vinigodoy.clustering.knn.Knn;
-import br.com.vinigodoy.clustering.type.color.ImageDatasource;
-import br.com.vinigodoy.clustering.type.color.RGB;
-import br.com.vinigodoy.clustering.type.color.RGBSolver;
+import br.com.vinigodoy.clustering.data.image.ImageDatasource;
+import br.com.vinigodoy.clustering.data.image.RGB;
+import br.com.vinigodoy.clustering.data.image.RGBSolver;
 
+/**
+ * Shows how to use knn to classify an image in 3 classes.
+ */
 public class KnnWithImage {
     private static final int PLANT = 1;
     private static final int SOIL = 2;
     private static final int SKY = 3;
 
     public static void main(String[] args) throws Exception {
-        //Cloud class
+        //Load the image using an image data source
         final var ds = ImageDatasource.load("./images/field.jpg");
         final var name = "./images/knn-field";
+
+        //Create the image output. The datasource provide an output with the same dimensions of the loaded image.
         try (var out = ds.createOutput(name)) {
+            //Create a knn with an RGB Solver.
             final var knn = new Knn<>(new RGBSolver());
-            //Classes creation
+
+            //Classes creation. These colors where manually chosen in the image
+
             //Vegetation
-            knn.addSample(PLANT, new RGB(90, 130, 63));
-            knn.addSample(PLANT, new RGB(150, 193, 113 ));
-            knn.addSample(PLANT, new RGB(41, 47, 39));
-            knn.addSample(PLANT, new RGB(168, 166, 107));
+            knn.addSamples(PLANT,
+                new RGB(90, 130, 63),
+                new RGB(150, 193, 113 ),
+                new RGB(41, 47, 39),
+                new RGB(168, 166, 107)
+            );
 
             //Soil
-            knn.addSample(SOIL, new RGB(244, 174, 129));
-            knn.addSample(SOIL, new RGB(224, 151, 97 ));
-            knn.addSample(SOIL, new RGB(230, 212, 191));
+            knn.addSamples(SOIL,
+                new RGB(244, 174, 129),
+                new RGB(224, 151, 97 ),
+                new RGB(230, 212, 191)
+            );
 
             //Sky (including clouds)
-            knn.addSample(SKY, new RGB(255, 254, 255));
-            knn.addSample(SKY, new RGB(127, 169, 210 ));
-            knn.addSample(SKY, new RGB(59, 130, 195));
-            knn.addSample(SKY, new RGB(196, 210, 231));
+            knn.addSamples(SKY,
+                new RGB(255, 254, 255),
+                new RGB(127, 169, 210 ),
+                new RGB(59, 130, 195),
+                new RGB(196, 210, 231)
+            );
 
-            //Label colors in output
-            out.setLabelColor(SKY, 0x0000FF);
-            out.setLabelColor(PLANT, 0x00FF00);
-            out.setLabelColor(SOIL, 0xFF0000);
+            //Label colors in output. If you ommit this lines, random colors will be used.
+            out.setLabelColor(SKY, 0x0000FF) //Blue
+                .setLabelColor(PLANT, 0x00FF00) //Green
+                .setLabelColor(SOIL, 0xFF0000); //Red
 
+            //Since several pixels have the same color, and RGB is a cacheable data type, activate the cache to reduce
+            //classification effort.
             knn.setCached(true)
-                    .classify(ds, out);
+                .classify(ds, out); //Do the classification and save it to the output
         }
     }
 }
